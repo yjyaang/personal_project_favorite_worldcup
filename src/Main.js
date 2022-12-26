15ba2bf38e954f02891dccff3dfc8b53c30c1e7b
yjyaang <topbuty97@gmail.com>
2022-12-26 15:06:33 +0900

    initial_setting

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Main = () => {
+    return (
+        <div>
+            <div>카트라이더 리그 명장면 월드컵</div>
+            <button>Start</button>
+        </div>
+    )
+}
+
+export default Main
